44da012a98d8509991e75e3b6cdb09294581805e
Ujjawal Singh <iamujjawal10.us@gmail.com>
2020-10-26 16:27:30 +0530

    Combing Multiple Reducers

--- redux--01-start/src/store/reducers/counter.js
+++ redux--01-start/src/store/reducers/counter.js
@@ -1,8 +1,7 @@
-import * as actionTypes from './actions';
+import * as actionTypes from '../actions';
 
 const initialState = {
     counter: 0,
-    results: []
 }
 
 const reducer = (state = initialState, action) => {
@@ -26,18 +25,6 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 counter: state.counter - action.val
             }
-        case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({id: new Date(), value: state.counter})
-            }
-        case actionTypes.DELETE_RESULT:
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return {
-                ...state,
-                results: updatedArray
-            }
-
     }
     return state;
 };
