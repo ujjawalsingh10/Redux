f005eab6f2d529eb7552320aee72e04f71724b49
Ujjawal Singh <iamujjawal10.us@gmail.com>
2020-10-31 19:18:12 +0530

    Handling Asynchronous code (Redux Thunk)

--- redux--01-start/src/store/actions/actions.js
+++ redux--01-start/src/store/actions/actions.js
@@ -27,12 +27,22 @@ export const add  = (value) => {
         val: value
     };
 };
-export const storeResult  = (res) => {
-    return {
+
+export const saveResult = (res) => {
+   return {
         type: STORE_RESULT,
         result: res
     };
+}
+
+export const storeResult  = (res) => {
+    return dispatch => {
+        setTimeout( () => {
+            dispatch(saveResult(res));
+        }, 2000);
+    }
 };
+
 export const deleteResult  = (resElId) => {
     return {
         type: DELETE_RESULT,
